ee9800a9db10c3ba308dfdf85b6fa628ec12387a
qiao.li <qiao.li@chiancreator.com>
2017-02-09 09:37:49 +0800

    整合jp项目移动端接口

--- qysyb-common-workflow/qysyb-workflow-serviceapi/src/main/java/com/chinacreator/c2/qyb/workflow/activiti/impl/WorkProcessEasy.java
+++ qysyb-common-workflow/qysyb-workflow-serviceapi/src/main/java/com/chinacreator/c2/qyb/workflow/activiti/impl/WorkProcessEasy.java
@@ -1,5 +1,4 @@
 package com.chinacreator.c2.qyb.workflow.activiti.impl;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -11,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.alibaba.fastjson.JSONObject;
+import com.chinacreator.asp.comp.sys.core.security.service.AccessControlServiceImpl;
 import com.chinacreator.c2.dao.Dao;
 import com.chinacreator.c2.dao.DaoFactory;
 import com.chinacreator.c2.flow.detail.WfBusinessData;
@@ -25,7 +25,6 @@ import com.chinacreator.c2.qyb.workflow.form.impl.FormService;
 import com.chinacreator.c2.qyb.workflow.module.entity.ServiceProduct;
 import com.chinacreator.c2.qyb.workflow.module.impl.ServiceProductService;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
 @Component
 public class WorkProcessEasy {
 	@Autowired
@@ -40,7 +39,6 @@ public class WorkProcessEasy {
 	ServiceProductService serviceProductService;
 	@Autowired
 	ActivityConfigService acfs;
-
 	/**
 	 * 流程流转
 	 * 
@@ -50,14 +48,14 @@ public class WorkProcessEasy {
 	 * @param transition
 	 * @param handler
 	 * @param ccInformJsonStr
+	 * @return 
 	 * @throws Exception
 	 */
 	@SuppressWarnings("unchecked")
-	public void goAnyWhereTakeTransition(String userId, String taskId,
+	public Object goAnyWhereTakeTransition(String userId, String taskId,
 			String entity, String transition, String handler,
 			String ccInformJsonStr) throws Exception {
 		Map paramsMap = new HashMap();
-
 		Task task = taskService.createTaskQuery().taskId(taskId)
 				.includeProcessVariables().singleResult();
 		Map processVariables = task.getProcessVariables();
@@ -79,19 +77,26 @@ public class WorkProcessEasy {
 		// String transitionId = (String) paramsMap.get("transitionId");
 		// String transitionStr = (String) paramsMap.get("transition");
 		// String ccInformJsonStr = (String) paramsMap.get("ccInform");
-
+		if(userId == null){
+			AccessControlServiceImpl access = ApplicationContextManager.getContext().getBean(AccessControlServiceImpl.class);
+			userId = access.getUserID();
+		}
+		
 		Map entitymap = JSONObject.parseObject(entity, Map.class);
-
 		String formId = (String) processVariables.get("formId");
 		String businessKey = (String) processVariables.get("businessKey");
 		String moduleId = (String) processVariables.get("moduleId");
+		ServiceProduct serviceProduct = serviceProductService
+				.getServiceProductById(moduleId);
+		if( formId == null){
+			formId = serviceProduct.getFormId();
+		}		
 		WfOperator wfOperator = new WfOperator();
 		wfOperator.setUserId(userId);
 		WfBusinessData businessData = new WfBusinessData();
 		businessData.setBusinessKey(businessKey);
 		businessData.setModuleId(moduleId);
 		wfOperator.setBusinessData(businessData);
-
 		ObjectMapper mapper = new ObjectMapper();
 		String wfOperatorStr = mapper.writeValueAsString(wfOperator);
 		String currenTaskId = taskId;
@@ -107,6 +112,10 @@ public class WorkProcessEasy {
 		if (ccInformJsonStr == null) {
 			ccInformJsonStr = "{}";
 		}
+		Map entityold = formService.getFormDataByFkFromProcessVariable(formId,
+				null, businessKey, proInsId);
+		entityold.putAll(entitymap);
+		entity = new JSONObject(entityold).toString();
 		paramsMap.put("entity", entity);
 		paramsMap.put("formId", formId);
 		paramsMap.put("wfOperator", wfOperatorStr);
@@ -122,9 +131,8 @@ public class WorkProcessEasy {
 		paramsMap.put("transitionId", transitionId);
 		paramsMap.put("transition", transition);
 		paramsMap.put("ccInform", ccInformJsonStr);
-		workProcess.goAnyWhereTakeTransition(paramsMap);
+		return workProcess.goAnyWhereTakeTransition(paramsMap);
 	}
-
 	/**
 	 * 获取一个任务处理前置信息
 	 * 
@@ -138,28 +146,39 @@ public class WorkProcessEasy {
 				.includeProcessVariables().singleResult();
 		Map processVariables = task.getProcessVariables();
 		String proInsId = task.getProcessInstanceId();
-
-		Map entity = formService.getFormDataByFkFromProcessVariable(taskId,
-				null, taskId, taskId);
-		result.put("entity", entity);
-		String formId = (String) processVariables.get("formId");
+		String productNo = (String) processVariables.get("productNo");
 		String moduleId = (String) processVariables.get("moduleId");
+		String formId = (String) processVariables.get("formId");
+		ServiceProduct serviceProduct = serviceProductService
+				.getServiceProductById(moduleId);
+		if( formId == null){
+			formId = serviceProduct.getFormId();
+		}
 		String businessKey = (String) processVariables.get("businessKey");
 		String processDefinitionId = task.getProcessDefinitionId();
 		String taskDefId = task.getTaskDefinitionKey();
-
-		ServiceProduct serviceProduct = serviceProductService
-				.getServiceProductById(moduleId);
-		List<FormField> fields = this.getFields(serviceProduct, businessKey);
+		Map entity = formService.getFormDataByFkFromProcessVariable(formId,
+				null, businessKey, proInsId);
+		result.put("entity", entity);
+		List<FormField> fields = this.getFields(serviceProduct, taskDefId);
 		result.put("fields", fields);
 		List<WorkFlowTransition> transitions = workFlowService
 				.getOutTransition(null, processDefinitionId, taskDefId);
+		//获取下一步的节点的一些属性
+		for(int i=0;i<transitions.size();i++){
+			WorkFlowTransition wft = transitions.get(i);
+			WorkFlowActivity destActivity = wft.getDest();
+			String destActId = destActivity.getId();
+			Map destActions = acfs.getActivityActions(moduleId, destActId);
+			Map proper = destActivity.getPorperties();
+			proper.put("actions", destActions);
+			destActivity.setPorperties(proper);
+		}
 		result.put("transitions", transitions);
 		Map actions = acfs.getActivityActions(moduleId, taskDefId);
 		result.put("actions", actions);
 		return result;
 	}
-
 	/**
 	 * 获取需要的字段。。
 	 * 
@@ -184,8 +203,10 @@ public class WorkProcessEasy {
 			fp.setModuleId(serviceProduct.getProductId());
 			fp.setBusinessKey(businessKey);
 			fp = daofp.selectOne(fp);
-			if (fp.isFillNecessary()
-					&& ff.getWebDisplayTypeId().equals("AUDIT_OPINION")) {
+			if (fp != null && fp.isFillNecessary()
+					&& (ff.getWebDisplayTypeId().equals("AUDIT_OPINION")
+					|| ff.getWebDisplayTypeId().equals("FW_COUNTERSIGN")
+					|| ff.getWebDisplayTypeId().equals("FW_SHYJ"))) {
 				listffresult.add(ff);
 			}
 			/*
@@ -200,7 +221,6 @@ public class WorkProcessEasy {
 		}
 		return listffresult;
 	}
-
 	@SuppressWarnings("unchecked")
 	public void goAnyWhere(String userId, String taskId, String entity,
 			String destTaskDefinitionKey, boolean useHisassigne,
@@ -221,19 +241,16 @@ public class WorkProcessEasy {
 		// String proInsId = (String) paramsMap.get("proInsId");
 		// String transitionStr = (String) paramsMap.get("transition");
 		// String curActivityStr = (String) paramsMap.get("curActivity");
-
 		Map entitymap = JSONObject.parseObject(entity, Map.class);
 		Task task = taskService.createTaskQuery().taskId(taskId)
 				.includeProcessVariables().singleResult();
 		Map processVariables = task.getProcessVariables();
-
 		String formId = (String) processVariables.get("formId");
 		String businessKey = (String) processVariables.get("businessKey");
 		String moduleId = (String) processVariables.get("moduleId");
 		String processDefinitionId = task.getProcessDefinitionId();
 		String opinion = entitymap.get("opinion") == null ? ""
 				: (String) entitymap.get("opinion");
-
 		WorkFlowActivity wfact = new WorkFlowActivity();
 		wfact.setId(task.getTaskDefinitionKey());
 		wfact.setName(task.getName());
@@ -247,7 +264,6 @@ public class WorkProcessEasy {
 		businessData.setModuleId(moduleId);
 		wfOperator.setBusinessData(businessData);
 		String wfOperatorStr = mapper.writeValueAsString(wfOperator);
-
 		paramsMap.put("entity", entity);
 		paramsMap.put("formId", formId);
 		paramsMap.put("businessKey", businessKey);
@@ -260,5 +276,67 @@ public class WorkProcessEasy {
 		paramsMap.put("curActivity", curActivityStr);
 		paramsMap.put("wfOperator", wfOperatorStr);
 		workProcess.goAnyWhere(paramsMap);
+	}
+	
+	@SuppressWarnings("unchecked")
+	public Object rejectGoAnyWhere(String taskId,
+			String entity,String destTaskDefinitionKey) throws Exception{
+		Map paramsMap = new HashMap();
+		Task task = taskService.createTaskQuery().taskId(taskId)
+				.includeProcessVariables().singleResult();
+		Map entitymap = JSONObject.parseObject(entity, Map.class);
+		Map processVariables = task.getProcessVariables();
+		String formId = (String) processVariables.get("formId");
+		String businessKey = (String) processVariables.get("businessKey");
+		String moduleId = (String) processVariables.get("moduleId");
+		String processDefinitionId = task.getProcessDefinitionId();
+		String proInsId = task.getProcessInstanceId();
+		ServiceProduct serviceProduct = serviceProductService
+				.getServiceProductById(moduleId);
+		String userId = null;
+		if(userId == null){
+			AccessControlServiceImpl access = ApplicationContextManager.getContext().getBean(AccessControlServiceImpl.class);
+			userId = access.getUserID();
+		}
+		if( formId == null){
+			formId = serviceProduct.getFormId();
+		}	
+		WfOperator wfOperator = new WfOperator();
+		wfOperator.setUserId(userId);
+		WfBusinessData businessData = new WfBusinessData();
+		businessData.setBusinessKey(businessKey);
+		businessData.setModuleId(moduleId);
+		wfOperator.setBusinessData(businessData);
+		ObjectMapper mapper = new ObjectMapper();
+		String wfOperatorStr = mapper.writeValueAsString(wfOperator);
+		WorkFlowActivity wfact = new WorkFlowActivity();
+		wfact.setId(task.getTaskDefinitionKey());
+		wfact.setName(task.getName());
+		// wfact.setPorperties(task.);
+		String curActivityStr = mapper.writeValueAsString(wfact);
+		String opinion = entitymap.get("opinion") == null ? ""
+				: (String) entitymap.get("opinion");
+//		WorkFlowTransition wfTransition = JSONObject.parseObject(transition,
+//				WorkFlowTransition.class);
+//		String destTaskDefinitionKey = wfTransition.getDest().getId();
+		
+		paramsMap.put("entity", entity);
+		paramsMap.put("formId", formId);
+		paramsMap.put("wfOperator", wfOperatorStr);
+		paramsMap.put("businessKey", businessKey);
+		paramsMap.put("currenTaskId", taskId);
+		paramsMap.put("destTaskDefinitionKey", destTaskDefinitionKey);
+		paramsMap.put("processDefinitionId", processDefinitionId);
+		paramsMap.put("currenTaskId", taskId);
+		//paramsMap.put("variables", handler);
+		paramsMap.put("opinion", opinion);
+		paramsMap.put("proInsId", proInsId);
+		//paramsMap.put("moduleId", moduleId);
+		//paramsMap.put("transitionId", transitionId);
+		paramsMap.put("curActivity", curActivityStr);
+		//paramsMap.put("transition", transition);
+		//paramsMap.put("ccInform", ccInformJsonStr);
+		
+		return workProcess.goAnyWhere(paramsMap);
 	}
 }
