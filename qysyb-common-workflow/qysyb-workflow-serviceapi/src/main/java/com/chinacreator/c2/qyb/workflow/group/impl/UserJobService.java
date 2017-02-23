6f509ef61267665f5c93013de1822e132c967ac4
qiao.li <qiao.li@chiancreator.com>
2017-02-23 10:15:40 +0800

    处理人到机构组 选择下一步处理人处理

--- qysyb-common-workflow/qysyb-workflow-serviceapi/src/main/java/com/chinacreator/c2/qyb/workflow/group/impl/UserJobService.java
+++ qysyb-common-workflow/qysyb-workflow-serviceapi/src/main/java/com/chinacreator/c2/qyb/workflow/group/impl/UserJobService.java
@@ -5,11 +5,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.chinacreator.asp.comp.sys.advanced.job.service.JobService;
 import com.chinacreator.asp.comp.sys.advanced.user.service.UserService;
 import com.chinacreator.asp.comp.sys.basic.org.dto.OrgDTO;
 import com.chinacreator.asp.comp.sys.core.user.dto.UserDTO;
-import com.chinacreator.c2.ioc.ApplicationContextManager;
+import com.chinacreator.c2.dao.Dao;
+import com.chinacreator.c2.dao.DaoFactory;
+import com.chinacreator.c2.qyb.workflow.usergroup.entity.ActIdMembership;
+import com.chinacreator.c2.qyb.workflow.usergroup.entity.ActIdUser;
+import com.chinacreator.c2.qyb.workflow.usergroup.entity.ActIdUserMapper;
 /**
  * 
  * @author Work
@@ -18,19 +21,25 @@ import com.chinacreator.c2.ioc.ApplicationContextManager;
 @Service
 public class UserJobService {
 
-//	UserService userService = ApplicationContextManager.getContext().getBean(UserService.class);
 	/**
 	 * 获取工作组用户
 	 * @param jobid
 	 * @return
 	 */
 	public List<UserDTO> getAllUserJob(String jobid){
-		JobService jobService = ApplicationContextManager.getContext().getBean(JobService.class);
-		List<UserDTO> list = jobService.queryUsers(jobid);
- 		for(int i=0;i<list.size();i++){
- 			list.get(i).setUserPassword(null);
+		Dao<Object> dao = DaoFactory.create(Object.class);
+		ActIdUserMapper mapper = dao.getSession().getMapper(ActIdUserMapper.class);
+		List<ActIdUser> list = mapper.getActIdUsersByGroup(jobid);
+		List<UserDTO> result = new ArrayList<UserDTO>();
+ 		for(ActIdUser actIdUser:list){
+ 			UserDTO ud = new UserDTO();
+ 			ud.setUserId(actIdUser.getId());
+ 			ud.setUserName(actIdUser.getFirst());
+ 			ud.setUserRealname(actIdUser.getLast());
+ 			ud.setUserEmail(actIdUser.getEmail());
+ 			result.add(ud);
  		}		
-		return list;
+		return result;
 	}
 	@Autowired
 	UserService userService;
@@ -42,13 +51,21 @@ public class UserJobService {
 	 */
 	public List<UserDTO> getAllUserFromJobWithSameOrg(String jobId,String curUserId){
 		List<UserDTO> result = new ArrayList<UserDTO>();
-		List<UserDTO> list = this.getAllUserJob(jobId);
+		
+		Dao<Object> dao = DaoFactory.create(Object.class);
+		ActIdUserMapper mapper = dao.getSession().getMapper(ActIdUserMapper.class);
+		List<ActIdUser> list = mapper.getActIdUsersByGroup(jobId);		
+		
 		OrgDTO con = userService.queryMainOrg(curUserId);
-		for(UserDTO userDTO:list){
-			OrgDTO org = userService.queryMainOrg(userDTO.getUserId());
+		for(ActIdUser actIdUser:list){
+			OrgDTO org = userService.queryMainOrg(actIdUser.getId());
 			if(con.getOrgId().equals(org.getOrgId())){
-				userDTO.setUserPassword(null);
-				result.add(userDTO);
+	 			UserDTO ud = new UserDTO();
+	 			ud.setUserId(actIdUser.getId());
+	 			ud.setUserName(actIdUser.getFirst());
+	 			ud.setUserRealname(actIdUser.getLast());
+	 			ud.setUserEmail(actIdUser.getEmail());
+	 			result.add(ud);
 			}
 		}
 		return result;
